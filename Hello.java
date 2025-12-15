27f01eeffc19b7b069282ef0679e0f80f2ca5873
MDADIL -1234 <er.mdadil95@gmail.com>
2025-12-15 20:12:13 +0530

    frist commit

---
+++ Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+int aa=60; int bb=60;
+		
+ System.out.println(aa==bb);//true
+ System.out.println(aa!=bb);// false
+ System.out.println(aa>=bb);// true
+                    
+ 
+		
+
+	}
+
+} 
